0d2c8c1c5d0e82481f4fbf9a5088ebe1ae93a277
Zabi-PW <syed.ulla2@pw.live>
2024-02-11 16:10:20 +0530

    Modified after cloning

--- Launch.java
+++ Launch.java
@@ -4,4 +4,12 @@ class Launch
 	{
 		System.out.println("Exploring Git & GitHub!");
 	}
+
+	public static void fun()
+	{
+		int a = 10;
+		int b = 20;
+		int sum = a+b;
+		System.out.println(sum);
+	}
 }
